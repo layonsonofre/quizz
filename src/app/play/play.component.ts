5047d57f88cd75b9dd147d01478eb76049c96e51
Layon de Souza Onofre <layonsonofre@gmail.com>
2019-04-07 20:39:23 -0300

    [WIP] Now we can play the game

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -15,6 +15,11 @@ export class PlayComponent implements OnInit {
   quizz: any;
   playing: boolean;
   question: number;
+  finish: boolean;
+  corrects: number;
+  deadline: number;
+  time: number;
+  results: any[];
 
   constructor(private activatedRoute: ActivatedRoute, private router: Router, private dataService: DataService, public utils: UtilsService) { }
 
@@ -41,9 +46,12 @@ export class PlayComponent implements OnInit {
   }
 
   start() {
+    this.deadline = undefined;
     this.question = 0;
+    this.time = 0;
     this.sort();
     this.playing = true;
+    this.finish = false;
   }
 
   sort() {
@@ -52,7 +60,11 @@ export class PlayComponent implements OnInit {
   }
 
   randomAnswers() {
-    this.quizz.questions[this.question].answers.sort((a, b) => a.text - b.text);
+    for (let i = this.quizz.questions[this.question].answers.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.quizz.questions[this.question].answers[i], this.quizz.questions[this.question].answers[j]] = [this.quizz.questions[this.question].answers[j], this.quizz.questions[this.question].answers[i]];
+    }
+    this.choose(this.quizz.questions[this.question].answers[0]);
   }
 
   stop() {
@@ -61,6 +73,49 @@ export class PlayComponent implements OnInit {
 
   next() {
     this.question += 1;
+    let deadlineIndex = this.question - 1;
+    if (this.question >= this.quizz.questions.length) {
+      this.checkResult();
+    } else {
+      this.randomAnswers();
+    }
+    this.time += this.deadline == 0 ? this.quizz.questions[deadlineIndex].deadline : (this.quizz.questions[deadlineIndex].deadline - this.deadline);
+  }
 
+  choose(answer: any) {
+    if (typeof this.deadline == 'undefined' || this.deadline > 0) {
+      answer.selected = !answer.selected;
+      this.quizz.questions[this.question].answers.forEach(a => {
+        if (a.text != answer.text) {
+          a.selected = false;
+        }
+      });
+    }
+  }
+
+  checkResult() {
+    this.corrects = this.quizz.questions.map(item => item.answers.filter(a => a.selected)[0]).filter(item => item.correct).length;
+    this.results = [];
+    for (let i = 0; i < this.quizz.questions.length; i++) {
+      this.results.push({question: this.quizz.questions[i].question});
+      for (let j = 0; j < this.quizz.questions[i].answers.length; j++) {
+        if (this.quizz.questions[i].answers[j].selected) {
+          this.results[i].user = this.quizz.questions[i].answers[j].text;
+        }
+        if (this.quizz.questions[i].answers[j].correct) {
+          this.results[i].correct = this.quizz.questions[i].answers[j].text;
+        }
+      }
+    }
+    this.finish = true;
+  }
+
+  setDeadline(count: number) {
+    this.deadline = count;
+    if (count == 0) {
+      setTimeout(() => {
+        this.next();
+      }, 3000);
+    }
   }
 }
